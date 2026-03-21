95f255632e24ebe7f4990ce6971ddd88209cc6b9
jujuliju <main.juli.ju@gmail.com>
2026-03-21 10:10:03 +0100

    fixed picture

--- Pico-code/scripts/oled_images.py
+++ Pico-code/scripts/oled_images.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from numpy import *
 
-path = 'Pictures/logo.png'
+path = 'Pictures/oledlogo.png'
 oleddim = array([128,64])
 
 img = Image.open(path)
@@ -9,7 +9,7 @@ dims = array([img.width, img.height])
 ratio = dims/oleddim
 ar = dims[1] / dims[0]
 ndims = (oleddim[0], int(ar*oleddim[0])) if ratio[0] > ratio[1] else (int(oleddim[1]/ar), oleddim[1])
-img = img.resize(ndims, Image.Resampling.NEAREST).convert('L')
+img = img.resize(ndims, Image.Resampling.HAMMING).convert('L')
 
 img.show()
 
@@ -17,5 +17,5 @@ output = open(path[:-3]+'csv', 'w')
 pixels = img.load()
 for x in range(ndims[0]):
     for y in range(ndims[1]):
-        v = pixels[x,y]/255
+        v = 0 if pixels[x,y] > 125 else 1
         output.write(f'{x},{y},{v}\n')
