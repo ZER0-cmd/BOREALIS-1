5bcc0eab86fcdb52ad91ef5048ff2baf4ef262e1
jujuliju <main.juli.ju@gmail.com>
2026-06-10 21:39:30 +0200

    ahhh

--- scripts/time_set.py
+++ scripts/time_set.py
@@ -1,8 +1,12 @@
+# Run: mpremote run scripts/time_set.py (Get-Date -Format "yyyy MM dd w HH mm ss")
+
+
 from machine import Pin, I2C
 from drivers.rtc_ds3231 import DS3231
-from time import gmtime, strftime
+import sys
 
-TIME = strftime("%Y %m %d %a %H %M %S", gmtime()).split()
+# "%Y %m %d %w %H %M %S"
+TIME = sys.argv
 
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 rtc_ext = DS3231(i2c)
