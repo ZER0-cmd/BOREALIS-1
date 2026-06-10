3405540887a6a3bb51acb4a9b01ef6a56f84333a
jujuliju <main.juli.ju@gmail.com>
2026-06-10 11:28:51 +0200

    set time script

--- Pico-code/scripts/time_set.py
+++ Pico-code/scripts/time_set.py
@@ -1,21 +1,12 @@
 from machine import Pin, I2C
 from drivers.rtc_ds3231 import DS3231
+from time import gmtime, strftime
 
-# ==== EDIT THESE TO CURRENT UTC ====
-YEAR   = 2025
-MONTH  = 12
-DAY    = 6
-HOUR   = 12  # 24h UTC
-MINUTE = 12
-SECOND = 0
-# ===================================
-
-# weekday doesn’t really matter much; set to 1 (Monday) if you don’t care
-WEEKDAY = 6
+TIME = strftime("%Y %m %d %a %H %M %S", gmtime()).split()
 
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 rtc_ext = DS3231(i2c)
 
-rtc_ext.datetime((YEAR, MONTH, DAY, WEEKDAY, HOUR, MINUTE, SECOND, 0))
+rtc_ext.datetime((*TIME, 0))
 
 print("DS3231 set to:", rtc_ext.datetime())
